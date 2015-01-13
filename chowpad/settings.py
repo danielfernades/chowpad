b4e4d2f0f012857d11493e0d354fad37b7956a92
marc condon <marc@sfdjango.com>
2015-01-13 11:16:50 -0800

    added django nose, 'cart' tests ok, added django-extensions

--- chowpad/settings.py
+++ chowpad/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+    'django_extensions',
+    'cart',
+
 )
 
 MIDDLEWARE_CLASSES = (
