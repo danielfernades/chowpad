bdc5706ad6cdb1d0cd787d03b5b7e26b0b5700bd
marc condon <marc@sfdjango.com>
2015-01-13 22:38:03 -0800

    finished work flow upto place order

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,8 +1,10 @@
 __author__ = 'marc'
-__author__ = 'marc'
+import re
+import operator
 from cart import Cart
 from cart import ItemAlreadyExists
-from menu.models import Entree as Product
+from menu.models import Entree
+from orders.models import Otable
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
@@ -13,5 +15,73 @@ from orders.models import Order
 
 
 def place_order(request):
-    print request.POST
+
+    item_count = 0
+    name_pat = 'name-\d+-'
+    quantity_pat = 'quantity-\d+-'
+    price_pat = 'price-\d+-'
+    sides_pat = 'sides-\d+-'
+    config_pat = 'configs-\d+-'
+    items = []
+    quantities = []
+    prices = []
+    sides = []
+    configs = []
+    table_name = request.POST['table-name']
+
+
+    # get sides from post
+    for i in request.POST.keys():
+        if re.match(sides_pat, i):
+            score = i.split('-')[1]
+            sides.append({'value': request.POST[i], 'score': score})
+    sides = sorted(sides, key=operator.itemgetter('score'))
+
+    # get configs from post
+    for i in request.POST.keys():
+        if re.match(config_pat, i):
+            score = i.split('-')[1]
+            configs.append({'value': request.POST[i], 'score': score})
+    configs = sorted(configs, key=operator.itemgetter('score'))
+
+    # get prices from post
+    for i in request.POST.keys():
+        if re.match(price_pat, i):
+            score = i.split('-')[1]
+            prices.append({'value': request.POST[i], 'score': score})
+    prices = sorted(prices, key=operator.itemgetter('score'))
+
+
+    # get quantities from post
+    for i in request.POST.keys():
+        if re.match(quantity_pat, i):
+            score = i.split('-')[1]
+            quantities.append({'value': request.POST[i], 'score': score})
+    quantities = sorted(quantities, key=operator.itemgetter('score'))
+
+    # get names from post
+    for i in request.POST.keys():
+        if re.match(name_pat, i):
+            score = i.split('-')[1]
+            item_count += 1
+            items.append({'value': request.POST[i], 'score': score})
+    items = sorted(items, key=operator.itemgetter('score'))
+    j = 0
+    for i in items:
+        order = Order()
+        order.otable = Otable.objects.get(name=table_name)
+        order.entree = Entree.objects.get(name=items[j]['value'])
+        order.quantity = quantities[j]['value']
+        order.price = prices[j]['value']
+        try:
+            order.sides = sides[j]['value']
+        except IndexError:
+            pass
+        try:
+            order.configurations = configs[j]['value']
+        except IndexError:
+            pass
+
+        order.save()
+        j += 1
     pass
--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,9 @@
 __author__ = 'marc'
-
+from datetime import datetime as dt
 from django.db import models
 
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from menu.models import Entree
 
 
@@ -25,8 +27,8 @@ class Order(models.Model):
     """
     otable = models.ForeignKey(Otable)
 
-    entrees = models.ManyToManyField(Entree)
-    status = models.TextField(max_length = 100, null=True, blank=True, choices=Order_statuses)
+    entree = models.ForeignKey(Entree)
+    status = models.TextField(max_length = 100, null=True, blank=True, choices=Order_statuses, default="Cooking")
     quantity = models.IntegerField()
 
     price = models.DecimalField(max_digits=5, null=True, blank=True, decimal_places=2)
@@ -34,8 +36,23 @@ class Order(models.Model):
     sides = models.TextField(max_length = 100, null=True, blank=True)
     configurations = models.TextField(max_length = 100, null=True, blank=True)
 
+    created_at = models.DateField(null=True)
+    updated_at = models.DateField(null=True)
     def __str__(self):              # __unicode__ on Python 2
         return '%s'%(self.entree.name)
 
     class Meta:
-        ordering = ('entree__name',)
+        ordering = ('-updated_at',)
+
+
+
+
+@receiver(pre_save, sender=Order)
+def entree_pre_save_handler(sender, **kwargs):
+    """
+    change update date
+    :param sender:
+    :param kwargs:
+    :return:
+    """
+    kwargs['instance'].updated_at = dt.now()
