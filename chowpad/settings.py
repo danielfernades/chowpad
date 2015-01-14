173892c7dbf4e43c29df6ddd3010b146fef090c7
marc condon <marc@sfdjango.com>
2015-01-13 20:31:05 -0800

    added place order workflow

--- chowpad/settings.py
+++ chowpad/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_nose',
     'django_extensions',
-    'chowcart',
     'menu',
     'orders',
 )
--- orders/views.py
+++ orders/views.py
@@ -5,7 +5,11 @@ from menu.models import Entree as Product
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
+from django.template import RequestContext
+from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from menu.models import Entree
+
 def add_to_cart(request, menu_name, table_name, entree_id):
     quantity=1
     product = Product.objects.get(id=entree_id)
@@ -14,22 +18,60 @@ def add_to_cart(request, menu_name, table_name, entree_id):
         cart.add(product, product.price, quantity)
     except ItemAlreadyExists:
     # change quantity if exist
-
         for e in cart:
             if e.product.name==product.name:
-                print 'found q: %s'%(e.quantity)
                 quantity = e.quantity + 1
                 price = e.product.price
             break
-        print 'new q: %s'%(quantity)
         # cart update not working
         cart.remove(product)
         cart.add(product, price, quantity)
     return HttpResponseRedirect(reverse('select_table', kwargs={'menu_name': menu_name,
                                                              'table_name': table_name }))
 
-def remove_from_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
+def remove_from_cart(request, menu_name, table_name, product_name):
+    product = Product.objects.get(name=product_name)
     cart = Cart(request)
     cart.remove(product)
+    return HttpResponseRedirect(reverse('select_table', kwargs={'menu_name': menu_name,
+                                                             'table_name': table_name }))
 
+def check_out(request, menu_name, table_name):
+    """
+    Second checkout setup for selecting sides and configurations
+    :param request:
+    :return:
+    """
+    #
+    # do lookups to marry configurations and sides
+    #  into order_items
+    #
+    # order_items to be rendered into form in template
+    order_items = []
+    cart = Cart(request)
+    j = 0
+    for i in cart:
+        e = Entree.objects.get(name=i.product.name)
+        sides = e.sides.split()
+        configs = e.configurations.split()
+        io = {
+            'index': j,
+            'name': e.name,
+            'quantity': i.quantity,
+            'price': e.price,
+            'sides': sides,
+            'configs': configs
+        }
+        order_items.append(io)
+        j += 1
+    return render(request, 'orders/check_out.html',
+        {
+            'menu_name': menu_name,
+            'table_name': table_name,
+            'cart': cart,
+            'order_items': order_items,
+        },
+        context_instance=RequestContext(request))
+
+def place_order(request):
+    pass
